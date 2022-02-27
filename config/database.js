7a3079a8fb0c6204839c1080ce28a08a2ec633e7
Daniel Asante <baremetals16@gmail.com>
2022-02-27 09:43:05 +0000

    master changes begin

--- config/database.js
+++ config/database.js
@@ -1,26 +1,13 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST"),
-//       port: env.int("DATABASE_PORT"),
-//       database: env("DATABASE_NAME"),
-//       user: env("DATABASE_USERNAME"),
-//       password: env("DATABASE_PASSWORD"),
-//       ssl: env.bool("DATABASE_SSL"),
-//     },
-//   },
-// });
-
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
     },
   },
 });
