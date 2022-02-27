3a91d71c603695f6fa65475f71b990a9ddf20685
Daniel Asante <baremetals16@gmail.com>
2022-02-27 13:01:48 +0000

    config change

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'a927e84e3144467ad0344e691fcfacee'),
+    secret: env("ADMIN_JWT_SECRET", "a927e84e3144467ad0344e691fcfacee"),
+  },
+  // serveAdminPanel: env.bool("SERVE_ADMIN", true),
+  forgotPassword: {
+    from: env("EMAIL_FROM"),
+    replyTo: env("EMAIL_FROM"),
   },
 });
