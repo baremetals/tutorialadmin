a7ac14f1670f6c6f6c965ab46588b668d4bd5afc
Daniel Asante <baremetals16@gmail.com>
2022-02-28 16:22:28 +0000

    deployment prep

--- config/admin.js
+++ config/admin.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     salt: env("API_TOKEN_SALT"),
   },
   auth: {
-    secret: env("ADMIN_JWT_SECRET", "a927e84e3144467ad0344e691fcfacee"),
+    secret: env("ADMIN_JWT_SECRET"),
   },
-  // serveAdminPanel: env.bool("SERVE_ADMIN", true),
+  
   forgotPassword: {
     from: env("EMAIL_FROM"),
     replyTo: env("EMAIL_FROM"),
